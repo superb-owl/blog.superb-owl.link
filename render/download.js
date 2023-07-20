bdb693741b52d21e0ed0beab331024167b1419ae
Max Goodbird <owl@superb-owl.link>
2023-07-20 08:33:28 -0400

    fix render

--- render/download.js
+++ render/download.js
@@ -23,7 +23,7 @@ export async function getPosts() {
 
 export async function fillDetails(post) {
   console.log(post.story_permalink);
-  post.id = p.story_permalink.replace(/^.*\/p\/([\w-]+).*$/, '$1');
+  post.id = post.story_permalink.replace(/^.*\/p\/([\w-]+).*$/, '$1');
   const resp = await fetch(post.story_permalink);
   if (resp.status === 404) {
     return false
