405ae2239db844242f07f02f543d60639c5f1a6e
Max Goodbird <owl@superb-owl.link>
2023-08-02 10:12:34 -0400

    fix up iframe heights

--- render/post.js
+++ render/post.js
@@ -45,7 +45,7 @@ export function renderPost(post) {
     </a>
   `);
   $('a[href="https://superbowl.substack.com/subscribe"]').replaceWith(`
-  <iframe class="subscribe-embed" src="https://superbowl.substack.com/embed" height="80" width="480" frameborder="0" scrolling="no"></iframe>
+  <iframe class="subscribe-embed" src="https://superbowl.substack.com/embed" frameborder="0" scrolling="no"></iframe>
   `);
   $("h1").each(function() {
     $(this).attr('id', $(this).text().replaceAll(/\W+/g, '-').toLowerCase())
